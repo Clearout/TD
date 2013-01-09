21531b06fcfccc2875fd4565c117ad895ba6a4f4
Clearout <joacimjakobsen@gmail.com>
2013-01-10 00:54:24 +0100

    0.1

--- src/com/example/towerdefence/Game.java
+++ src/com/example/towerdefence/Game.java
@@ -1,5 +1,7 @@
 package com.example.towerdefence;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import android.os.Bundle;
@@ -12,7 +14,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 
 public class Game extends Activity implements Runnable {
 	private WakeLock wakeLock;
@@ -22,6 +28,11 @@ public class Game extends Activity implements Runnable {
 	private SurfaceView surfaceView;
 	private SurfaceHolder surfaceHolder;
 	private Screen screen;
+	private Canvas canvas = null;
+	Rect src = new Rect();
+	Rect dst = new Rect();
+	private Bitmap offscreenSurface;
+	private int offscreenWidth, offscreenHeight;
 
 	@SuppressWarnings("deprecation")
 	public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,18 @@ public class Game extends Activity implements Runnable {
 		surfaceView = new SurfaceView(this);
 		setContentView(surfaceView);
 		surfaceHolder = surfaceView.getHolder();
+		screen = createStartScreen();
+		setOffscreenSurfaceOrientation();
+	}
+	
+	public Screen createStartScreen() {
+		return null;
+	}
+	
+	public void setScreen(Screen screen) {
+		if (this.screen != null)
+			screen.dispose();
+		this.screen = screen;
 	}
 
 	public void onPause() {
@@ -63,14 +86,17 @@ public class Game extends Activity implements Runnable {
 				for (int i=0; i<stateChanges.size(); i++) {
 					state = stateChanges.get(i);
 					if (state == State.Disposed) {
+						screen.dispose();
 						Log.d("Game", "disposed");
 						return;
 					}
 					if (state == State.Paused) {
+						screen.pause();
 						Log.d("Game", "paused");
 						return;
 					}
 					if (state == State.Resumed) {
+						screen.resume();
 						Log.d("Game", "resumed");
 					}
 				}
@@ -79,12 +105,81 @@ public class Game extends Activity implements Runnable {
 			if (state == State.Running) {
 				if (!surfaceHolder.getSurface().isValid()) 
 					continue;
-				Canvas canvas = surfaceHolder.lockCanvas();
-				
+				canvas = surfaceHolder.lockCanvas();
+				if (screen != null) 
+					screen.update(0);
+				src.left = 0;
+				src.top = 0;
+				src.right = offscreenSurface.getWidth() - 1;
+				src.bottom = offscreenSurface.getHeight() - 1;
+				dst.left = 0;
+				dst.top = 0;
+				dst.right = offscreenSurface.getWidth();
+				dst.bottom = offscreenSurface.getHeight();
+				canvas.drawBitmap(offscreenSurface, src, dst, null);
 				surfaceHolder.unlockCanvasAndPost(canvas);
-				
 			}
 		}
 	}
-
+	
+	public void clearFrameBuffer(int color) {
+		if (canvas != null)
+			canvas.drawColor(color);
+	}
+	
+	public int getFrameBufferWidth() { return offscreenSurface.getWidth(); }
+	public int getFrameBufferHeight() { return offscreenSurface.getHeight(); }
+	
+	public Bitmap loadBitmap(String fileName) {
+		InputStream in = null;
+		Bitmap bitmap = null;
+		try {
+			in = getAssets().open(fileName);
+			bitmap = BitmapFactory.decodeStream(in);
+			if (bitmap == null)
+				throw new RuntimeException ("Couldnt find the file from asset " + fileName);
+			return bitmap;
+		} catch(IOException e) {
+			throw new RuntimeException ("Couldnt find the file from asset " + fileName);
+		} finally{
+			if (in != null) try { in.close(); } catch(IOException e) {}
+		}
+	}
+	
+	public void drawBitmap(Bitmap bitmap, int x, int y) {
+		if (canvas != null) 
+			canvas.drawBitmap(bitmap, x, y, null);
+	}
+	
+	public void drawBitmap(Bitmap bitmap, int x, int y, int srcX, int srcY, int srcWidth, int srcHeight) {
+		if (canvas == null)
+			return;
+		src.left = srcX;
+		src.top = srcY;
+		src.right = srcX + srcWidth;
+		src.bottom = srcY + srcHeight;
+		
+		dst.left = x;
+		dst.top = y;
+		dst.right = x + srcWidth;
+		dst.bottom = y + srcHeight;
+		
+		canvas.drawBitmap(bitmap, src, dst, null);
+	}
+	public void setOffscreenSurfaceOrientation() {
+		if (surfaceView.getWidth() > surfaceView.getHeight()) {
+			offscreenWidth = 1280;
+			offscreenHeight = 720;
+		} else {
+			offscreenWidth = 720;
+			offscreenHeight = 1280;
+		}
+		setOffscreenSurface(offscreenWidth, offscreenHeight);
+	}
+	public void setOffscreenSurface(int width, int height) {
+		if (offscreenSurface != null) 
+			offscreenSurface.recycle();
+		offscreenSurface = Bitmap.createBitmap(width, height, Config.RGB_565);
+		canvas = new Canvas(offscreenSurface);
+	}
 }
--- src/com/example/towerdefence/Screen.java
+++ src/com/example/towerdefence/Screen.java
@@ -1,5 +1,14 @@
 package com.example.towerdefence;
 
-public class Screen {
-
+public abstract class Screen {
+	protected final Game game;
+	
+	public Screen(Game game) {
+		this.game = game;
+	}
+	
+	public abstract void update (float deltatime);
+	public abstract void pause();
+	public abstract void resume();
+	public abstract void dispose();
 }
