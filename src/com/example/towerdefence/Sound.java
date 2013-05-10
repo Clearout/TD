b7fa1d9dfce03fbc3dbf9a482526b9da22191844
Joacim André Jakobsen <joacimjakobsen@gmail.com>
2013-05-10 21:28:29 +0200

    Added necessary licenses to borrowed code

--- src/com/example/towerdefence/Sound.java
+++ src/com/example/towerdefence/Sound.java
@@ -1,7 +1,30 @@
 package com.example.towerdefence;
 
+/*******************************************************************************
+ * Copyright 2011 Mario Zechner
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 import android.media.SoundPool;
-
+/**
+ * Class abstracing a sound effect stored in a {@link SoundPool}. An instance
+ * of this class holds a reference to the SoundPool as well as the ID of the
+ * sound effect in the SoundPool for playback. Call the {@link Sound#dispose()}
+ * method when you no longer need the sound effect to clean up resources.
+ * 
+ * @author mzechner
+ *
+ */
 public class Sound {
 	int soundId;
 	SoundPool soundPool;
--- src/ui/Screen.java
+++ src/ui/Screen.java
@@ -1,3 +1,33 @@
+/*******************************************************************************
+ * Copyright 2011 Mario Zechner
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+/**
+ * <p>A Screen is one phase of a game, e.g. the main menu screen,
+ * the highscores screen and so on. A {@link Game} stores
+ * one currently active screen and will call its {@link Screen#update(float)}
+ * method on the main loop thread as often as possible. The Screen can then
+ * execute any logic in that method as well as render itself via the methods
+ * provided by the Game class.</p>
+ * <p>The Game class will also report any life-cycle events (pause/resume/dispose) to
+ * the currently active Screen</p>
+ * <p>Simply derrive from this class and implement the methods appropriately. Use
+ * {@link Game#setScreen(Screen)} to set a screen as the currently active screen.</p>
+ * @author mzechner
+ *
+ */
 package ui;
 
 import com.example.towerdefence.Game;
